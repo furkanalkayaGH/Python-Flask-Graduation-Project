1459645573ce204241b1c72c729724b0d4685cbe
Furkan Alkaya <furkan.alkayase@gmail.com>
2023-05-16 21:10:22 +0300

    Day 3 added new pages and setted up connections

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return "<p>Login</p>"
+    return render_template("login.html")
 
-@auth.route('/logout')
+@auth.route('/about')
 def logout():
-    return "<p>Logout</p>"
+    return render_template("about.html")
 
-@auth.route('/sign-up')
+@auth.route('/info')
 def sign_up():
-    return "<p>Sign Up</p>"
+    return render_template("info.html")
