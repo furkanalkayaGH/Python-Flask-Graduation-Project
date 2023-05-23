a30c39460b103518d16183fbd34e80e9d474cdaf
Furkan Alkaya <furkan.alkayase@gmail.com>
2023-05-23 11:43:49 +0300

    +added alerts -issue alert dismiss

--- website/auth.py
+++ website/auth.py
@@ -2,11 +2,11 @@ from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/sign-up', methods=['GET','POST'])
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def signup():
-    if request.method == ['POST']:
+    if request.method == 'POST':
         email = request.form.get('email')
-        firstName = request.form.get('firsName')
+        firstName = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
         
@@ -19,9 +19,9 @@ def signup():
         elif len(password1) < 7:
             flash('Password must be at least 7 characters.', category='error')
         else:
-            flash('Account created!', category='success')
-            
-
+            flash('Account created!', category='success')        
+    data = request.form
+    print(data)
     return render_template("signup.html")
 
 @auth.route('/sign-in', methods=['GET','POST'])
