70c75ea183375d9a6e1e8cc6c576366665661ca2
Furkan Alkaya <furkan.alkayase@gmail.com>
2023-06-09 21:40:48 +0300

    com

--- website/__init__.py
+++ website/__init__.py
@@ -16,15 +16,13 @@ def create_app():
     app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
     db.init_app(app)
 
-    
-
     from .views import views
     from .auth import auth
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
-    from .models import User, Note
+    from .models import User
 
     with app.app_context():
         db.create_all()
